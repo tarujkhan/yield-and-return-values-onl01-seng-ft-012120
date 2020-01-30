5dfcd897cb7881a320f968856d500440a19f0159
taroj khan <tarujkhan@yahoo.com>
2020-01-30 00:59:04 +0000

    Automatically backed up by Learn

--- lib/practicing_returns.rb
+++ lib/practicing_returns.rb
@@ -1,16 +1,12 @@
-#require 'pry'
+
 
 def hello(array)
   i = 0
   collection = []
   while i < array.length
   collection << yield(array[i])
-  #binding.pry
-    #yield(array[i])
     i += 1
   end
   collection
 end
 
-#binding.pry
-#hello(["Tim", "Tom", "Jim"]) { |name| "Hi, #{name}" }
